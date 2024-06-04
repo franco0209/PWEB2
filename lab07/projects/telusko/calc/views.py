e7746bbd1ed04e5d7b629cc4fa03413edc0872d6
Franco Jesus Cahua Soto <fcahua@unsa.edu.pe>
2024-06-04 10:08:37 -0500

    Se termina la funcion add y se culmina satisfactoriamente la pagina result que suma 2 datos obtenidos por formulario

--- lab07/projects/telusko/calc/views.py
+++ lab07/projects/telusko/calc/views.py
@@ -6,4 +6,8 @@ def home(request):
     return render(request, "home.html",{"name":"Kiran"})
 
 def add(request):
-    return render(request, "result.html")
+    val1=int(request.GET["num1"])
+    val2=int(request.GET["num2"])
+    result=val1+val2 
+
+    return render(request, "result.html",{"result":result})
