0ae359533144d17a3564181bfb36e3783375464f
Franco Jesus Cahua Soto <fcahua@unsa.edu.pe>
2024-06-07 10:54:00 -0500

    Relacion Query one to many, se modifican los modelos añadiendo metodos nuevos

--- lab08/projects/model_example/example/models.py
+++ lab08/projects/model_example/example/models.py
@@ -19,9 +19,15 @@ class NullExample(models.Model):
 class Language(models.Model):
     name=models.CharField(max_length=10)
 
+    def _str_(self):
+        return self.name
+
 class Framework(models.Model):
     name=models.CharField(max_length=10)
     language=models.ForeignKey(Language, on_delete=models.CASCADE)
+      
+    def _str_(self):
+        return self.name
 
 class Extra(models.Model):
     name=models.CharField(max_length=1)
