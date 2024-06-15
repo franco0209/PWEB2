e265ef2f010fd6448f6dce2cfbf51490a17b3d71
Franco Jesus Cahua Soto <fcahua@unsa.edu.pe>
2024-06-15 10:45:00 -0500

    Añadiendo relacion manyToMany Query, modificando las clases Character y Movie y metodos str

--- lab08/projects/model_example/example/models.py
+++ lab08/projects/model_example/example/models.py
@@ -10,8 +10,6 @@ class Simple(models.Model):
     def __str__(self):
         return self.url
 
-class DataExample(models.Model):
-    the_date=models.DateTimeField()
 
 class NullExample(models.Model):
     col=models.CharField(max_length=10,blank=True, null=True)
@@ -31,14 +29,13 @@ class Framework(models.Model):
 
 class Movie(models.Model):
     name=models.CharField(max_length=10)
-
-    def _str_(self):
+    def __str__(self):
         return self.name
     
 class Character(models.Model):
     name=models.CharField(max_length=10)
     movies= models.ManyToManyField(Movie)
 
-    def _str_(self):
+    def __str__(self):
         return self.name    
      
